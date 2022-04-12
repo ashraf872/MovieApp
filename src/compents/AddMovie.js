0ce2bed2415717ee6b5394d488d5adfb26128dbf
Ashraf Ben Khemis <85137443+ashraf872@users.noreply.github.com>
2022-04-12 12:19:14 +0100

    finalone

--- src/compents/AddMovie.js
+++ src/compents/AddMovie.js
@@ -1,72 +1,106 @@
-import React, { useState } from 'react'
-import Modal from 'react-modal';
-import StarRating from './StarRating';
-
+import React, { useState } from "react";
+import Modal from "react-modal";
+import StarRating from "./StarRating";
 const customStyles = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
   },
 };
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
-const AddMovie = ({handelAdd}) => {
-    const [modalIsOpen, setIsOpen] = useState(false);
-    const [name, setName] = useState('');
-    const [date, setDate] = useState('');
-    const [image, setImage] = useState('');
-    const [rating, setRating] = useState('2');
-    function openModal() {
-        setIsOpen(true);
-      }
-      function closeModal() {
-        setIsOpen(false);
-      }
-      // func off stars
-      const handelRating =(r)=>setRating(r);
-      // func off movies
-     const handelSumbit = (e)=>{
-     e.preventDefault();
-     const newMovie={
-      id :Math.random(),
-      name : name ,
-      date : date ,
-      image : image ,
-     };        
-     handelAdd(newMovie)
-    }
-    return (
-        <div>
-            <button className='btn add-movie-btn' onClick={openModal}>Add movie </button>
+const AddMovie = ({ handelAdd }) => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  // const [name, setName] = useState("");
+  // const [imageUrl, setImageURL] = useState("");
+  // const [date, setDate] = useState("");
+  // const [rate, setRate] = useState(1);
+
+  const [form, setForm] = useState({
+    name: "",
+    date: "",
+    image: "",
+    rating: 1,
+  });
+  // handle change in the form input
+  const handelChange = (e) =>
+    setForm({ ...form, [e.target.name]: e.target.value });
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  // handle rating
+  const handelRate = (rate) => setForm({ ...form, rating: rate });
+  return (
+    <div>
+      <button className="btn add-movie-btn" onClick={openModal}>
+        Add movie
+      </button>
       <Modal
+        style={customStyles}
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
-        style={customStyles}
+        contentLabel="Example Modal"
       >
-          <form className='' onSubmit={handelSumbit}>
-              <label>Name</label>
-              <input type='text' value={name} onChange={(e)=> setName(e.target.value)}/>
-              <label>Date</label>
-              <input type='Date' value={date}  onChange={(e)=> setDate(e.target.value)} />
-              <label>Rating</label>
-              <StarRating rating={rating} handelRating={handelRating}/>
-              <label>Poste</label>
-              <input required type='URL' value={image} onChange={(e)=> setImage(e.target.value)} />
-              <div>
-                <button className="btn btn-primary" type='submit'>confirm</button>
-                <button className="btn btn-primary" onClick={closeModal}>cancel</button>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            let newMovie = {
+              ...form,
+              id: Math.random(),
+            };
+          handelAdd(newMovie);
+            closeModal();
+          }}
+        >
+          <label>Name</label>
+          <input
+            type="text"
+            value={form.name}
+            name="name"
+            onChange={handelChange}
+            // required
+          />
+          <label> Date of release </label>
+          <input
+            type="date"
+            value={form.date}
+            name="date"
+            onChange={handelChange}
+            // required
+          />
+          <label> Rating </label>
+          {/* <StarRating rate={form.rating} handelRating={handelRate} /> */}
+          <StarRating rating={form.rating} handelRating={handelRate}/>
 
-              </div>
-              
-          </form>
+          <label>Image</label>
+          <input
+            type="url"
+            value={form.image}
+            name="image"
+            onChange={handelChange}
+            // required
+          />
+          <div>
+            <button className="btn btn-primary" type="submit">
+              Add
+            </button>
+            <button className="btn btn-danger" onClick={closeModal}>
+              Cancel
+            </button>
+          </div>
+        </form>
       </Modal>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
-export default AddMovie
+export default AddMovie;
